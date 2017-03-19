e3f3ba915cb9f0c662f0ddcd67f85775ea8fd6a3
Barbara Fusinska <barbara.fusinska@gmail.com>
2017-03-19 19:29:39 +0000

    Adjust classification of irises to the Classification process

--- Datasets/Iris/Classification.R
+++ Datasets/Iris/Classification.R
@@ -1,26 +1,35 @@
-# Split data to train and test
-install.packages('caTools')
-library(caTools)
-
-tt.datasets <- split.dataset(iris, .75)
+# Splitting dataset to train and test
+tt.datasets <- split.dataset(iris, iris$Species, .75)
 iris.train <- tt.datasets$train
 iris.test <- tt.datasets$test
 
 summary(iris.train)
 summary(iris.test)
 
+# Train the model
+
+# Naive Bayes
+nb.model <- train.model(iris.train, "nb", "Species")
+
 # kNN
+knn.model <- train.model(iris.train, "knn", "Species")
 
-library(class)
-knnModel <- knn(iris.train[,1:4], iris.test[,1:4], iris.train[,5], k = 3, prob = TRUE)
-knnModel
-summary(knnModel)
+# Scores for labels
 
-install.packages("gmodels")
-library(gmodels)
+# Naive Bayes
+lb.nb.train <- predict.labels(nb.model, iris.train)
+lb.nb.test <- predict.labels(nb.model, iris.test)
 
-CrossTable(iris.test[,5], knnModel, prop.chisq = FALSE)
+# kNN
+lb.knn.train <- predict.labels(knn.model, iris.train, is.knn = TRUE)
+lb.knn.test <- predict.labels(knn.model, iris.test, is.knn = TRUE)
 
-library(ggplot2)
-library(caret) 
-classification.metrics(iris.test[,5], knnModel, 3)
+# Classification results
+
+# Naive Bayes
+classification.metrics(iris.train[,5], lb.nb.train, 3)
+classification.metrics(iris.test[,5], lb.nb.test, 3)
+
+# kNN
+classification.metrics(iris.train[,5], lb.knn.train, 3)
+classification.metrics(iris.test[,5], lb.knn.test, 3)
